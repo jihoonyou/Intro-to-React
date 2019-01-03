663f040df199801a3eb8e1bf65ec01cc01f59496
jihoonyou <pianoetvoix@gmail.com>
2019-01-03 20:18:46 +0900

    Add calculateWinner

--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//하나의 button rendering
-class Square extends React.Component {
-  //lifting state up 했으니깐 더이상 핉요 X
-  // constructor(props) {
-  //   super(props);
-  //   //state를 가짐 밑에 설정했던 value i가 null값으로 초기세팅
-  //   this.state = {
-  //     value: null,
-  //   };
-  // } 
-  /***이제 Square components는 controlled componnets가 된 것!****/
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()} 
-      //onClick을 상위 컴포넌트에서 props으로 받아서 사용
-      >
-        {this.props.value}
-      </button>
+// //하나의 button rendering
+// class Square extends React.Component {
+//   //lifting state up 했으니깐 더이상 핉요 X
+//   // constructor(props) {
+//   //   super(props);
+//   //   //state를 가짐 밑에 설정했던 value i가 null값으로 초기세팅
+//   //   this.state = {
+//   //     value: null,
+//   //   };
+//   // } 
+//   **이제 Square components는 controlled componnets가 된 것!***
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()} 
+//       //onClick을 상위 컴포넌트에서 props으로 받아서 사용
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+//In React, function components are a simpler way to write components 
+//that only contain a render method and don’t have their own state. I
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
     );
-  }
 }
+
 //Board Component에 9개의 Squre rendering
 class Board extends React.Component {
   //lifting state up from Square component
@@ -30,6 +40,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -40,9 +51,15 @@ class Board extends React.Component {
     //2. Detecting changes in immutable objects is considerably easier
     //3. it helps you build pure components in React. Immutable data can easily determine 
     //if changes have been made which helps to determine when a component requires re-rendering.
-    const squares = this.sate.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares: squares,
+      xIsNext: !this.state.xIsNext,
+
+    });
   }
 
   renderSquare(i) {
@@ -53,8 +70,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -92,6 +114,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
